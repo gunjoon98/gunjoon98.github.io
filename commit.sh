2510d9b8e1e585b5eb7c5da0fb11232d9b979814
dlans321 <dlans321@naver.com>
2022-07-27 15:46:30 +0900

    commit

--- commit.sh
+++ commit.sh
@@ -2,16 +2,14 @@
 bundle exec jekyll build
 
 cd ../cache/
-rm -r *
+git rm -r *
 cp -r ../blog/_site/* .
 git add .
-git add -u
 git commit -m "commit"
 git push
 
 cd ../blog/
 git add .
-git add -u
 git commit -m "commit"
 git push
 
